c8a17ebd6eb91942b388f7ae32912a8c48270d6e
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-07 01:36:44 +0100

    styled for responsiveness across all devices

--- timbu-cloud-shop/app/components/SearchInput.tsx
+++ timbu-cloud-shop/app/components/SearchInput.tsx
@@ -9,15 +9,15 @@ const SearchInput = () => {
 				<input
 					type="text"
 					placeholder="Search"
-					className="pl-7 w-[400px] h-[35px] border border-[#08319C] rounded-md 
-							   focus:outline-none focus:border-[#08319C] text-sm mr-20"
+					className="pl-7 w-[200px] md:w-[400px] h-[35px] border border-[#08319C] rounded-md 
+					focus:outline-none focus:border-[#08319C] text-sm md:mr-20"
 				/>
 				<Search className="absolute w-4 h-4 left-2 text-[#00000080]" />
 			</div>
 
 			<div
 				className="w-[140px] h-[35px] border border-[#08319C] rounded-md flex text-center 
-							justify-center items-center"
+				justify-center items-center"
 			>
 				<span className="text-[16px] text-[#00000080]">My Account</span>
 				<ChevronDown className="w-4 h-4 ml-1" />
