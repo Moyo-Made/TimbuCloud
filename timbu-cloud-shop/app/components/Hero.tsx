b06769ae2731990b167c754a0f2ed7b3ad90eebb
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-08 00:52:46 +0100

    feat: added the hero section

---
+++ timbu-cloud-shop/app/components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import HeroImage from "@/public/hero-image.png";
+import Image from "next/image";
+
+const Hero: React.FC = () => {
+	return (
+		<div className="grid justify-center items-center">
+			<div className="mt-3">
+				<Image
+					src={HeroImage}
+					alt="Hero Image"
+					className="md:w-[1080px] w-[380px]"
+				/>
+				<div className="hidden md:block w-[1080px] h-[20px] bg-[#08319C26] mt-3"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
