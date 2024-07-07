61ee4b4dc615b45a2d727335137193a9ebd218dc
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-07 01:38:04 +0100

    added file that conatins the navlinks

---
+++ timbu-cloud-shop/app/navLinks.ts
@@ -0,0 +1,24 @@
+export interface NavLink {
+	name: string;
+	path: string;
+}
+
+export const navLinks: NavLink[] = [
+	{
+		name: "Products",
+		path: "/products",
+	},
+	{
+		name: "Contact",
+		path: "/products",
+	},
+	{
+		name: "Affiliate",
+		path: "/products",
+	},
+	{
+		name: "Special",
+		path: "/products",
+	},
+];
+
