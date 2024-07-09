3b7658cae04845c0485450103ad414257ed47ac0
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-09 22:54:57 +0100

    added file for handling cart states

---
+++ timbu-cloud-shop/app/stores/useCartStore.ts
@@ -0,0 +1,44 @@
+import create from "zustand";
+import { StaticImageData } from "next/image";
+
+interface Product {
+  name: string;
+  originalPrice: number;
+  discountedPrice: number;
+  description: string;
+  color: string;
+  model: string;
+  SN: string;
+  image: StaticImageData;
+  quantity: number;
+}
+
+interface CartState {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (index: number) => void;
+  updateQuantity: (index: number, newQuantity: number) => void;
+  isInCart: (productName: string) => boolean; // Corrected return type for isInCart
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  cart: [],
+  addToCart: (product) =>
+    set((state) => ({
+      cart: [...state.cart, { ...product, quantity: 1 }],
+    })),
+  removeFromCart: (index) =>
+    set((state) => ({
+      cart: state.cart.filter((_, i) => i !== index),
+    })),
+  updateQuantity: (index, newQuantity) =>
+    set((state) => ({
+      cart: state.cart.map((item, i) =>
+        i === index ? { ...item, quantity: newQuantity } : item
+      ),
+    })),
+    isInCart: (productName) => {
+      const { cart } = get();
+      return cart.some((item) => item.name === productName);
+    }, 
+}));
