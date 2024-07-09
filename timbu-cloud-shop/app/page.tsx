b7f5ea5dd245d3745694e1b346173b5d048dca3a
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-09 22:56:26 +0100

    updates home routing page

--- timbu-cloud-shop/app/page.tsx
+++ timbu-cloud-shop/app/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import SearchInput from "./components/SearchInput";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import FeaturedProducts from "./components/FeaturedProducts";
+import Footer from "./components/Footer";
 
 const page = () => {
 	return (
 		<div>
 			<SearchInput />
 			<Navbar />
+			<Hero />
+			<FeaturedProducts />
+			<Footer />
 		</div>
 	);
 };
