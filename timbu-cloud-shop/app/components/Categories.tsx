e6a9fd1486da0bfe7b5883a81c4d398dd481842f
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-09 22:52:29 +0100

    added categories section

--- timbu-cloud-shop/app/components/Categories.tsx
+++ timbu-cloud-shop/app/components/Categories.tsx
@@ -12,57 +12,65 @@ const Categories = () => {
 						Shop by category
 					</span>
 				</div>
-				<div className="flex justify-between items-center h-[35px] bg-[#08319C33] text-[#08319C] text-start">
-					<span className="text-[15px] font-medium ml-3">Smartphones</span>
+				<div className="flex justify-between items-center h-[35px] bg-[#08319C33] hover:bg-[#08319C] hover:text-[#ffff] text-[#08319C] text-start">
+					<a href="" className="text-[15px] font-medium ml-3">
+						Smartphones
+					</a>
 					<ChevronRight className="mr-2" />
 				</div>
 
-				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] text-[#08319C] text-start">
-					<span className="text-[15px] font-medium ml-3">Laptops</span>
+				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] hover:bg-[#08319C] hover:text-[#ffff] text-[#08319C] text-start">
+					<a href="" className="text-[15px] font-medium ml-3">
+						Laptops
+					</a>
 					<ChevronRight className="mr-2" />
 				</div>
 
-				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] text-[#08319C] text-start">
-					<span className="text-[15px] font-medium ml-3">Accessories</span>
+				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] hover:bg-[#08319C] hover:text-[#ffff] text-[#08319C] text-start">
+					<a href="" className="text-[15px] font-medium ml-3">
+						Accessories
+					</a>
 					<ChevronRight className="mr-2" />
 				</div>
 
-				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] text-[#08319C] text-start">
-					<span className="text-[15px] font-medium ml-3">Gaming</span>
+				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] hover:bg-[#08319C] hover:text-[#ffff] text-[#08319C] text-start">
+					<a href="" className="text-[15px] font-medium ml-3">
+						Gaming
+					</a>
 					<ChevronRight className="mr-2" />
 				</div>
 
-				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] text-[#08319C] text-start">
-					<span className="text-[15px] font-medium ml-3">
+				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] hover:bg-[#08319C] hover:text-[#ffff] text-[#08319C] text-start">
+					<a href="" className="text-[15px] font-medium ml-3">
 						Wearable Technologies
-					</span>
+					</a>
 					<ChevronRight className="mr-2" />
 				</div>
 
-				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] text-[#08319C] text-start">
-					<span className="text-[15px] font-medium ml-3">
+				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] hover:bg-[#08319C] hover:text-[#ffff] text-[#08319C] text-start">
+					<a href="" className="text-[15px] font-medium ml-3">
 						Photo/Videography
-					</span>
+					</a>
 					<ChevronRight className="mr-2" />
 				</div>
 
-				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] text-[#08319C] text-start">
-					<span className="text-[15px] font-medium ml-3">
+				<div className="flex justify-between items-center h-[35px] mt-[3px] bg-[#08319C33] hover:bg-[#08319C] hover:text-[#ffff] text-[#08319C] text-start">
+					<a href="" className="text-[15px] font-medium ml-3 ">
 						Audio Entertainment
-					</span>
+					</a>
 					<ChevronRight className="mr-2" />
 				</div>
 			</div>
 
 			{/* For Mobile */}
 			<div>
-				<div className="md:hidden grid w-[380px] h-[35px] ml-10 -mt-6 bg-[#08319C] text-[#FFFFFF] text-center justify-center items-center">
+				<div className="md:hidden grid w-[370px] h-[35px] ml-4 -mt-6 bg-[#08319C] text-[#FFFFFF] text-center justify-center items-center">
 					<span className="uppercase text-[16px] font-medium">
 						Shop by category
 					</span>
 				</div>
 
-				<div className="grid grid-cols-2 ml-10">
+				<div className="grid grid-cols-2 ml-4">
 					<div className="md:hidden flex justify-start items-center w-[180px] h-[35px] mt-2 bg-[#08319C33] text-[#08319C] text-start">
 						<span className="text-[15px] font-medium pl-8">Smartphones</span>
 						<ChevronDown className="ml-2" />
@@ -83,14 +91,14 @@ const Categories = () => {
 						<ChevronDown className="ml-2" />
 					</div>
 
-					<div className="md:hidden flex justify-start items-center w-[220px] h-[35px] mt-2 bg-[#08319C33] text-[#08319C] text-start">
+					<div className="md:hidden flex justify-start items-center w-[210px] h-[35px] mt-2 bg-[#08319C33] text-[#08319C] text-start">
 						<span className="text-[15px] font-medium pl-8">
 							Photo/Videography
 						</span>
 						<ChevronDown className="ml-2" />
 					</div>
 
-					<div className="md:hidden flex justify-start items-center w-[150px] h-[35px] ml-6 mt-2 bg-[#08319C33] text-[#08319C] text-start">
+					<div className="md:hidden flex justify-start items-center w-[140px] h-[35px] ml-8 mt-2 bg-[#08319C33] text-[#08319C] text-start">
 						<span className="text-[15px] font-medium pl-8">Gaming</span>
 						<ChevronDown className="ml-2" />
 					</div>
