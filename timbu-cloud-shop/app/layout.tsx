7c0712f75e84420a51ebc5ad616a8640d7bd5658
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-09-02 16:31:28 +0100

    updated layout

--- timbu-cloud-shop/app/layout.tsx
+++ timbu-cloud-shop/app/layout.tsx
@@ -1,27 +1,24 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "@/styles/globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Timbu Cloud Shop",
-  description: "An E-commerce Product Listing Platform",
+	title: "Timbu Cloud Shop",
+	description: "An E-commerce Product Listing Platform",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="icon" href="/favicon.ico" />
+
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
